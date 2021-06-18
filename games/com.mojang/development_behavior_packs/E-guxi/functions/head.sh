64f6d73e6fb95c0c4865f3327ceaa6a847ff8e2a
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-06-19 03:28:06 +0800

    remember head sh

--- games/com.mojang/development_behavior_packs/E-guxi/functions/head.sh
+++ games/com.mojang/development_behavior_packs/E-guxi/functions/head.sh
@@ -1,11 +1,11 @@
 path_to_head(){
-  sed -E -e "s@^${wd}/(.*)@#\1@" -e 's/\.mcfunction$//' <<EOM
-$path
+  sed -E 's@^'"${wd}"'/(.*)\.mcfunction$@\1@' <<EOM
+$*
 EOM
 }
 
-wd=$(realpath "$(pwd)")
-cd -P "$wd"
+cd -P .
+wd="${PWD}"
 main() {
   find -type f -name "*.mcfunction" | while read -r; do
     replace &
@@ -14,34 +14,30 @@ main() {
 }
 
 replace() {
-  mtime=$(stat -c %Y "$REPLY")
-
-  path=$(realpath "$REPLY")
-
-  if [ -z "$(cat "$REPLY")" ]; then
-    path_to_head >"$REPLY"
+  if [ ! -s "$REPLY" ]; then
+    {
+      printf "#"
+      path_to_head "$REPLY"
+    } > "$REPLY"
     return
   fi
 
-  head=$(head -n 1 "$REPLY")
+  mtime=$(stat -c %Y "$REPLY")
   
-  fpath="${wd}/$(printf "$head" | sed -E 's/^#(.*)$/\1.mcfunction/')"
+  pathHead=$(path_to_head "$REPLY")
   
-  if [ "$fpath" != "$path" ]; then
-    nmtime=$(stat -c %Y "$REPLY")
-    if [ "$mtime" != "$nmtime" ]; then
-      printf "文件“$path”被修改，跳过\n"
-      return
-    fi
+  fileHead=$(sed -nE '1s/^#(.*)/\1/p' "$REPLY")
 
+  if [ "$pathHead" != "$fileHead" ]; then
     tmp=$(mktemp)
     cat "$REPLY" > "$tmp"
-    if [[ $head =~ ^#.* ]]; then
-      sed -i '1d' "$tmp"
+    sed -i "1s@^#.*@#${pathHead}@;1s@^[^#].*@#${pathHead}\n&@" "$tmp"
+    nmtime=$(stat -c %Y "$REPLY")
+    if [ "$mtime" = "$nmtime" ]; then
+      cat "$tmp" > "$REPLY"
     fi
-    path_to_head > "$REPLY"
-    cat "$tmp" >> "$REPLY"
     rm -f "$tmp"
+    return
   fi
 }
 
