ba11311ba606307f60d0d9f9a5bb32d36e9cd6e2
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-07-29 00:33:33 +0800

    m

--- docs/sumIndex.sh
+++ docs/sumIndex.sh
@@ -1,36 +1,27 @@
 sumIndex(){
-  if [ -z "$prefix" ]; then
-    prefix="/"
-  elif [ "$prefix" = / ]; then
-    prefix="/${d}"
-  else
-    prefix="${prefix}/${d}"
-  fi
+  # 如果有html文档
+  # 结束
   if [[ -f index.html || -f index.htm ]]; then
     return
   fi
-
   {
-
     cat <<EOM
-# Index of $prefix
+# Index of ${prefix:-/}
 
 EOM
-
-    if [ "$prefix" != / ]; then
+    if [ -n "${prefix}" ]; then
       echo "[../](./../)  "
     fi
-
     for d in *; do
       if [ -d "${d}" ]; then
         (
+          prefix="${prefix}/${d}"
           cd "${d}"
           eval "$FUNCNAME"
         )
         echo "[${d}/](./${d}/)  "
       fi
     done
-
     for f in *; do
       if [ -f "${f}" ]; then
         t="${f##*.}"
@@ -44,11 +35,10 @@ EOM
         fi
       fi
     done
-
     if [ -f _index.txt ]; then
       cat _index.txt
     fi
   } > index.md
 }
-
+find -type f -name index.md -delete
 sumIndex
