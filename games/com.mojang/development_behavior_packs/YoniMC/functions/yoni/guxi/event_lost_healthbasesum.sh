3450eca9950dcac96d6c4abb20b3af30c5b1526a
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2022-03-16 23:28:28 +0800

    更改了能量损耗的计算

--- games/com.mojang/development_behavior_packs/YoniMC/functions/yoni/guxi/event_lost_healthbasesum.sh
+++ games/com.mojang/development_behavior_packs/YoniMC/functions/yoni/guxi/event_lost_healthbasesum.sh
@@ -2,26 +2,23 @@
 
 # sum damage.mcfunction
 file="event_lost_health.mcfunction"
-content='#dropenergyfunc
-#损失的能量: dropEnergy = 42 * lostHealth ^ 5 * k
-#k = (lastDisHealth+1)/maxHealth
+content='#lostenergyfunc
+#损失血量时丢失的能量:lostEnergy=disHealth^5
+
 '
 
 con=100000000
 health=60
 objective_energy="guxi:energy"
 objective_energies="guxi:energies"
-objective1="guxi:lostHealth"
-objective2="guxi:lstDisHea"
+objective1="guxi:disHealth"
 for((damage=1;damage<=health;damage++)); do
-  for((lastDisHealth=0;damage+lastDisHealth<=health;lastDisHealth++)); do
-    drop_energy=$((damage ** 5 * 42 * (lastDisHealth+1) / health))
+    drop_energy=$((damage ** 5 * 32))
     if ((drop_energy>con)); then
       drop_energies=$((drop_energy / con))
       drop_energy=$((drop_energy % con))
-      content+="execute @s[scores={${objective1}=${damage},${objective2}=${lastDisHealth}}] ~ ~ ~ scoreboard players remove @s ${objective_energies} ${drop_energies}"$'\n'
+      content+="execute @s[scores={${objective1}=${damage}}] ~ ~ ~ scoreboard players remove @s ${objective_energies} ${drop_energies}"$'\n'
     fi
-    content+="execute @s[scores={${objective1}=${damage},${objective2}=${lastDisHealth}}] ~ ~ ~ scoreboard players remove @s ${objective_energy} ${drop_energy}"$'\n'
-  done
+    content+="execute @s[scores={${objective1}=${damage}}] ~ ~ ~ scoreboard players remove @s ${objective_energy} ${drop_energy}"$'\n'
 done
 echo "$content" >"$file"
