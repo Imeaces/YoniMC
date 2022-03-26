d3eb1e6190f7811b327fb646b7173c8fc326b163
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2022-03-26 22:49:35 +0800

    增加Gametest模块

---
+++ games/com.mojang/development_behavior_packs/YoniMC/scripts/check.js
@@ -0,0 +1,19 @@
+import * as mc from "mojang-minecraft";
+var world = mc.world;
+var events = mc.world.events;
+
+events.tick.subscribe(event => {
+  dimensions = [
+    "overworld",
+    "nether",
+    "the end"
+  ];
+  for (dim in dimensions){
+    opt = new EntityQueryOptions();
+    opt.maxDistance = 10000000;
+    ents = world.getDimension(dim).getEntities(opt);
+    for (e in ents){
+      e.runCommand("say 执行了命令");
+    }
+  }
+});
